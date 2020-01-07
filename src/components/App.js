abf5a15098541b57efd0057a82321797ac48e292
병우 유 <conner903@naver.com>
2020-01-07 22:20:17 +0900

    Initialize React Project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>
+                    hello world
+                </h3>
+            </div>
+        )
+    }
+}
+
+export default App;
